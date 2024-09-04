cef6461fcb643ed1d501ae56c6f851b5c20e04ed
JordanWalling <jordanwalling1991@gmail.com>
2024-09-04 13:30:50 +1000

    Add general layout/styling to createGame page

--- app/dashboard/admin/create-game/page.js
+++ app/dashboard/admin/create-game/page.js
@@ -1,10 +1,82 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
-function createGame() {
+function CreateGame() {
+  const [homePlayers, setHomePlayers] = useState([]);
+  const [homePlayerName, setHomePlayerName] = useState("");
+  const [homePlayerTries, setHomePlayerTries] = useState("");
+  const [homePlayerTryAssists, setHomePlayerTryAssists] = useState("");
+  const [homePlayerTackles, setHomePlayerTackles] = useState("");
+  const [homePlayerMetresRun, setHomePlayerMetresRun] = useState("");
+  const [homePlayerTackleBreaks, setHomePlayerTackleBreaks] = useState("");
+  const [homePlayerLineBreaks, setHomePlayerLineBreaks] = useState("");
+  const [homePlayerMissedTackles, setHomePlayerMissedTackles] = useState("");
+
+  // away team
+  {
+    /* name, tries, tryAssists, tackles, metresRun, tackleBreaks, lineBreaks, missedTackles */
+  }
+  const [awayTeamPlayers, setAwayTeamPlayers] = useState([]);
+  const [awayPlayerName, setAwayPlayerName] = useState("");
+  const [awayPlayerTries, setAwayPlayerTries] = useState("");
+  const [awayPlayerTryAssists, setAwayPlayerTryAssists] = useState("");
+  const [awayPlayerTackles, setAwayPlayerTackles] = useState("");
+  const [awayPlayerMetresRun, setAwayPlayerMetresRun] = useState("");
+  const [awayPlayerTackleBreaks, setAwayPlayerTackleBreaks] = useState("");
+  const [awayPlayerLineBreaks, setAwayPlayerLineBreaks] = useState("");
+  const [awayPlayerMissedTackles, setAwayPlayerMissedTackles] = useState("");
+
+  const addHomePlayerStats = (e) => {
+    e.preventDefault();
+    const newPlayer = {
+      name: homePlayerName,
+      tries: homePlayerTries,
+      tryAssists: homePlayerTryAssists,
+      tackles: homePlayerTackles,
+      metresRun: homePlayerMetresRun,
+      tackleBreaks: homePlayerTackleBreaks,
+      lineBreaks: homePlayerLineBreaks,
+      missedTackles: homePlayerMissedTackles,
+    };
+    setHomePlayers([...homePlayers, newPlayer]);
+
+    // Clear input fields after submission
+    setHomePlayerName("");
+    setHomePlayerTries("");
+    setHomePlayerTryAssists("");
+    setHomePlayerTackles("");
+    setHomePlayerMetresRun("");
+    setHomePlayerTackleBreaks("");
+    setHomePlayerLineBreaks("");
+    setHomePlayerMissedTackles("");
+  };
+
+  const addAwayPlayer = (e) => {
+    e.preventDefault();
+    const newPlayer = {
+      name: awayPlayerName,
+      tries: awayPlayerTries,
+      tryAssists: awayPlayerTryAssists,
+      tackles: awayPlayerTackles,
+      metresRun: awayPlayerMetresRun,
+      tackleBreaks: awayPlayerTackleBreaks,
+      lineBreaks: awayPlayerLineBreaks,
+    };
+    setAwayTeamPlayers([...awayTeamPlayers, newPlayer]);
+    setAwayPlayerName("");
+    setAwayPlayerTries("");
+    setAwayPlayerTryAssists("");
+    setAwayPlayerTackles("");
+    setAwayPlayerMetresRun("");
+    setAwayPlayerTackleBreaks("");
+    setAwayPlayerLineBreaks("");
+    setAwayPlayerMissedTackles("");
+  };
+
   return (
     <div className="h-screen w-[100%] p-2 m-auto">
       {/* Form to create a new game */}
-
       <form
         className="bg-white p-4 border-2 border-slate-50 m-2 m-auto rounded-md flex flex-col cols-1 shadow-slate-400 
         shadow-xl"
@@ -37,17 +109,170 @@ function createGame() {
           <label className="uppercase text-xs mb-2 text-sky-600">
             Home Team
           </label>
-          <select name="" id="">
+          <select className="mb-4" name="" id="">
             <option value="Eagles">Manly Sea Eagles</option>
             <option value="Roosters">Sydney Roosters</option>
             <option value="Eels">Parramatta Eels</option>
             <option value="Panthers">Penrith Panthers</option>
           </select>
-          <form action="">
-            <label htmlFor="">Home Team Player Stats</label>
-            {/* ability to add individual player stats: tries, try assists, tackles, metres run, tackle breaks, line breaks, missed tackles */}
-          </form>
+          <div>
+            <label>Home Team Player Stats</label>
+
+            <div className="mt-4 border-slate-50 border-2 p-2 rounded-md">
+              <div className=" grid grid-cols-1 mb-3">
+                <label className="uppercase text-xs mb-1 text-sky-600">
+                  Player Name
+                </label>
+                <input
+                  type="text"
+                  value={homePlayerName}
+                  onChange={(e) => setHomePlayerName(e.target.value)}
+                  placeholder="Enter Player Name"
+                  className="focus:outline-none"
+                />
+              </div>
+              <div className=" grid grid-cols-1 mb-3">
+                <label className="uppercase text-xs mb-1 text-sky-600">
+                  Tries
+                </label>
+                <input
+                  type="number"
+                  value={homePlayerTries}
+                  onChange={(e) => setHomePlayerTries(e.target.value)}
+                  placeholder="Enter Tries"
+                  className="focus:outline-none"
+                />
+              </div>
+              <div className=" grid grid-cols-1 mb-3">
+                <label className="uppercase text-xs mb-1 text-sky-600">
+                  Try Assists
+                </label>
+                <input
+                  type="number"
+                  value={homePlayerTryAssists}
+                  onChange={(e) => setHomePlayerTryAssists(e.target.value)}
+                  placeholder="Enter Try Assists"
+                  className="focus:outline-none"
+                />
+              </div>
+              <div className=" grid grid-cols-1 mb-3">
+                <label className="uppercase text-xs mb-1 text-sky-600">
+                  Tackles
+                </label>
+                <input
+                  type="number"
+                  value={homePlayerTackles}
+                  onChange={(e) => setHomePlayerTackles(e.target.value)}
+                  placeholder="Enter Tackles"
+                  className="focus:outline-none"
+                />
+              </div>
+              <div className=" grid grid-cols-1 mb-3">
+                <label className="uppercase text-xs mb-1 text-sky-600">
+                  Metres Run
+                </label>
+                <input
+                  type="number"
+                  value={homePlayerMetresRun}
+                  onChange={(e) => setHomePlayerMetresRun(e.target.value)}
+                  placeholder="Enter Metres Run"
+                  className="focus:outline-none"
+                />
+              </div>
+              <div className=" grid grid-cols-1 mb-3">
+                <label className="uppercase text-xs mb-1 text-sky-600">
+                  Tackle Breaks
+                </label>
+                <input
+                  type="number"
+                  value={homePlayerTackleBreaks}
+                  onChange={(e) => setHomePlayerTackleBreaks(e.target.value)}
+                  placeholder="Enter Tackle Breaks"
+                  className="focus:outline-none"
+                />
+              </div>
+              <div className=" grid grid-cols-1 mb-3">
+                <label className="uppercase text-xs mb-1 text-sky-600">
+                  Line Breaks
+                </label>
+                <input
+                  type="number"
+                  value={homePlayerLineBreaks}
+                  onChange={(e) => setHomePlayerLineBreaks(e.target.value)}
+                  placeholder="Enter Line Breaks"
+                  className="focus:outline-none"
+                />
+              </div>
+              <div className=" grid grid-cols-1 mb-3">
+                <label className="uppercase text-xs mb-1 text-sky-600">
+                  Missed Tackles
+                </label>
+                <input
+                  type="number"
+                  value={homePlayerMissedTackles}
+                  onChange={(e) => setHomePlayerMissedTackles(e.target.value)}
+                  placeholder="Enter Missed Tackles"
+                  className="focus:outline-none"
+                />
+              </div>
+              <button
+                onClick={addHomePlayerStats}
+                className="mb-4 mt-2 bg-sky-600 text-white px-4 py-2 rounded"
+              >
+                Add Player Stats
+              </button>
+            </div>
+            <div className="overflow-x-auto">
+              <table className="w-[200%] table-fixed border-collapse">
+                <thead className="">
+                  <tr className="">
+                    <th className="p-2">Player Name</th>
+                    <th>Tries</th>
+                    <th>Try Assists</th>
+                    <th>Tackles</th>
+                    <th>Metres Run</th>
+                    <th>Tackle Breaks</th>
+                    <th>Line Breaks</th>
+                    <th>Missed Tackles</th>
+                  </tr>
+                </thead>
+                <tbody className="">
+                  {homePlayers.map((player, index) => {
+                    return (
+                      <tr key={index} className="">
+                        <td className="border px-2  py-1 text-center">
+                          {player.name}
+                        </td>
+                        <td className="border px-2 py-1 text-center">
+                          {player.tries}
+                        </td>
+                        <td className="border px-2 py-1 text-center">
+                          {player.tryAssists}
+                        </td>
+                        <td className="border px-2 py-1 text-center">
+                          {player.tackles}
+                        </td>
+                        <td className="border px-2 py-1 text-center">
+                          {player.metresRun}
+                        </td>
+                        <td className="border px-2 py-1 text-center">
+                          {player.tackleBreaks}
+                        </td>
+                        <td className="border px-2 py-1 text-center">
+                          {player.lineBreaks}
+                        </td>
+                        <td className="border px-2 py-1 text-center">
+                          {player.missedTackles}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
+
         <div className="flex flex-col cols-1 mb-4 border-0 ring-2 ring-slate-100 rounded px-2 py-2 focus:ring-3">
           <label className="uppercase text-xs mb-2 text-sky-600">
             Away Team
@@ -58,17 +283,125 @@ function createGame() {
             <option value="Eels">Parramatta Eels</option>
             <option value="Panthers">Penrith Panthers</option>
           </select>
-          <form action="">
-            <label htmlFor="">Home Team Player Stats</label>
-            {/* ability to add individual player stats: tries, try assists, tackles, metres run, tackle breaks, line breaks, missed tackles */}
-          </form>
+          <div>
+            <h2>Away Team Player Stats</h2>
+            {/* name, tries, tryAssists, tackles, metresRun, tackleBreaks, lineBreaks, missedTackles */}
+            <div>
+              <label htmlFor="">Player Name</label>
+              <input
+                type="text"
+                placeholder="Tom Trbojevic"
+                value={awayPlayerName}
+                onChange={(e) => setAwayPlayerName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Tries</label>
+              <input
+                type="number"
+                placeholder="2"
+                value={awayPlayerTries}
+                onChange={(e) => setAwayPlayerTries(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Try Assists</label>
+              <input
+                type="number"
+                placeholder="0"
+                value={awayPlayerTryAssists}
+                onChange={(e) => setAwayPlayerTryAssists(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Tackles</label>
+              <input
+                type="number"
+                placeholder="0"
+                value={awayPlayerTackles}
+                onChange={(e) => setAwayPlayerTackles(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Metres Run</label>
+              <input
+                type="number"
+                placeholder="0"
+                value={awayPlayerMetresRun}
+                onChange={(e) => setAwayPlayerMetresRun(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Tackle Breaks</label>
+              <input
+                type="number"
+                placeholder="0"
+                value={awayPlayerTackleBreaks}
+                onChange={(e) => setAwayPlayerTackleBreaks(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Line Breaks</label>
+              <input
+                type="number"
+                placeholder="0"
+                value={awayPlayerLineBreaks}
+                onChange={(e) => setAwayPlayerLineBreaks(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Missed Tackles</label>
+              <input
+                type="number"
+                placeholder="0"
+                value={awayPlayerMissedTackles}
+                onChange={(e) => setAwayPlayerMissedTackles(e.target.value)}
+              />
+            </div>
+            <button className="mb-3" onClick={addAwayPlayer}>
+              Add Player
+            </button>
+            <h3 className="mb-3">Away Player Stats</h3>
+            <div className="overflow-x-auto">
+              <table className="w-[200%] table-fixed border-collapse">
+                <thead>
+                  <tr>
+                    <th>Player Name</th>
+                    <th>Tries</th>
+                    <th>Try Assists</th>
+                    <th>Tackles</th>
+                    <th>Metres Run</th>
+                    <th>Tackle Breaks</th>
+                    <th>Line Breaks</th>
+                    <th>Missed Tackles</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {awayTeamPlayers.map((player, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{player.name}</td>
+                        <td>{player.tries}</td>
+                        <td>{player.tryAssists}</td>
+                        <td>{player.tackles}</td>
+                        <td>{player.metresRun}</td>
+                        <td>{player.tackleBreaks}</td>
+                        <td>{player.lineBreaks}</td>
+                        <td>{player.missedTackles}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
         <div className="">
           <button
             type="submit"
             className="mb-4 bg-sky-600 w-full rounded py-2 text-sky-100 uppercase shadow-lg shadow-slate-400"
           >
-            Create Team
+            Create Game
           </button>
         </div>
       </form>
@@ -82,4 +415,4 @@ function createGame() {
   );
 }
 
-export default createGame;
+export default CreateGame;
